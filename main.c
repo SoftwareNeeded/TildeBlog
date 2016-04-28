2e55e719cf574fd2c0094b851ec0769e25d6dc3a
minerobber <MineRobber9000@users.noreply.github.com>
2016-04-28 21:56:44 +0000

    Added ability to do more than 1 thing in 1 run.

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
+struct running { int val;
+	int retval;
+};
 void println(char * text);
+void mainloop(struct running *m);
 
 int main(int argc, char *argv[]) {
-	println("Welcome to TildeBlog! Choose an option!");
+	struct running m;
+	m.val = 1;
+	m.retval = 0;
+	println("Welcome to TildeBlog!");
+	while (m.val) {
+		mainloop(&m);
+	}
+	return m.retval;
+}
+
+void mainloop(struct running *m) {
+	char * sep = "---------------------";
+	println(sep);
 	println("1. Read your blog");
 	println("2. Write a post (NYI)");
 	println("3. Exit");
+	printf("Choice: ");
 	char choice[500];
 	scanf("%s", choice);
 	if (strcmp(choice,"3") == 0) {
-		return 0;
+		println(sep);
+		m->val = 0;
 	}
 	if (strcmp(choice,"2") == 0) {
 		println("NYI");
+		println(sep);
+		m->retval = -1;
+		m->val = 0;
 	}
 	if (strcmp(choice,"1") == 0) {
 		println("blahblahblah's blog");
@@ -24,7 +45,6 @@ int main(int argc, char *argv[]) {
 		println("This is a test to see if this works.");
 		println("Eventually, you will be able to set your own title and write your own posts.");
 	}
-	return 0;
 }
 
 void println(char * text) {
